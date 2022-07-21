905235b1d1ace91c6ccf8455ba20edba43f31286
Hedrekao <82450784+Hedrekao@users.noreply.github.com>
2022-07-21 21:30:37 +0200

    Correct look of past matches data (reverse order without months)

--- RestApiServer/api_routes/pastMatches.js
+++ RestApiServer/api_routes/pastMatches.js
@@ -1,37 +1,7 @@
 var fetch = require('node-fetch')
 var https = require('https')
 
-const getMonthName =  (monthNumber) =>
-{
-    switch(monthNumber)
-    {
-        case 1:
-            return "Styczeń"
-        case 2:
-            return "Luty"
-        case 3:
-            return "Marzec"
-        case 4:
-            return "Kwiecień"
-        case 5:
-            return "Maj"
-        case 6:
-            return "Czerwiec"
-        case 7:
-            return "Lipiec"
-        case 8:
-            return "Sierpień"
-        case 9:
-            return "Wrzesień"
-        case 10:
-            return "Październik"
-        case 11:
-            return "Listopad"
-        case 12:
-            return "Grudzień"
 
-    }
-}
 
 const fetchPastMatchesData = async function ()
 {
@@ -46,32 +16,9 @@ const fetchPastMatchesData = async function ()
     let i = 0;
     let j = 0;
     const resultObject = [];
-    let matchesThisMonth= [];
-    let currentMonth;
     for (const matchNumber in result) {
 
         if (result[matchNumber].ID_zespoly_gosc == '336' || result[matchNumber].ID_zespoly_gosp == '336') {
-
-            let monthNumber = parseInt(result[matchNumber].ZawodyData.split('.')[1])
-            if(j == 0 && i == 0 )
-            {
-                currentMonth = getMonthName(monthNumber)
-            }
-
-            if(getMonthName(monthNumber) != currentMonth)
-            {
-                resultObject[j] = {
-                    month: currentMonth,
-                    matches: matchesThisMonth
-                }
-
-                currentMonth = getMonthName(monthNumber)
-                i = 0;
-                j++;
-                matchesThisMonth = []
-            }
-            
-
 
             const matchData = await fetch(`https://rozgrywki.zprp.pl/api/pokaz_mecze_szczegoly.php?Zawody=${result[matchNumber].IdZawody
                 }`, { agent })
@@ -79,7 +26,7 @@ const fetchPastMatchesData = async function ()
             const matchDataJson = await matchData.json()
 
             // I THINK ITS NOT POSSIBLE GOALS AFTER FULL TIME IN CASE THERE WAS TIE
-            matchesThisMonth[i] = {
+            resultObject[i] = {
                 teamHome: matchDataJson[0][0].ID_zespoly_gosp_ZespolNazwa,
                 teamAway: matchDataJson[0][0].ID_zespoly_gosc_ZespolNazwa,
                 date: result[matchNumber].ZawodyData,
@@ -97,11 +44,7 @@ const fetchPastMatchesData = async function ()
         }
     }
 
-    resultObject[j] = {
-        month: currentMonth,
-        matches: matchesThisMonth
-    }
-
+    resultObject.reverse()
 
 
     console.log('past matches loaded')
